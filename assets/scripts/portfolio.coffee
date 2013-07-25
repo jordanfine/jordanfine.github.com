a3c2564963babd2e3b3281214b3c61b2ed11978c
Jordan Fine <jordan6fine@gmail.com>
2013-07-25 00:07:03 -0400

    Changed the design of the website, implementing CSS and working on placing all the content. STLL IN THE WORKS

--- assets/scripts/portfolio.coffee
+++ assets/scripts/portfolio.coffee
@@ -1,1 +1,62 @@
-$("#splash").backstretch "public/img/profile.jpg"
+$ ->
+	##$(".l-splash--image").backstretch "public/img/profile.png"
+
+	resize_top_image()
+	$(window).on "resize", resize_top_image
+	$("#header-about").on "click", (e) ->
+		e.preventDefault()
+		divY = $(".l-about").position().top
+		$("html,body").animate {scrollTop: divY}, 500
+
+	$("#learn-more").on "click", (e) ->
+		e.preventDefault()
+		divY = $(".l-about").position().top
+		$("html,body").animate {scrollTop: divY}, 500
+
+	$("#header-portfolio").on "click", (e) ->
+		e.preventDefault()
+		divY = $(".l-portfolio").position().top
+		$("html,body").animate {scrollTop: divY}, 500
+
+	$("#header-resume").on "click", (e) ->
+		e.preventDefault()
+		divY = $(".l-resume").position().top
+		$("html,body").animate {scrollTop: divY}, 500
+
+	$("#header-connect").on "click", (e) ->
+		e.preventDefault()
+		divY = $(".l-connect").position().top
+		$("html,body").animate {scrollTop: divY}, 500
+	
+###
+	$(window).on "scroll", ->
+		if($(@).scrollTop() >= threshold)
+			$(".l-header").fadeIn 500 , ->
+				$(@).removeClass "hide"
+		else
+			$(".l-header").fadeOut 200, ->
+				$(@).addClass "hide"
+###
+
+	
+
+
+threshold = 0
+
+resize_top_image = ->
+	windowHeight = $(window).height()
+	threshold = windowHeight
+	earlyAccessHeight = $(".l-splash--nav").height()
+
+	minHeight = parseInt($(".l-splash--image").css("min-height"))
+
+	###
+	if (windowHeight - earlyAccessHeight) > minHeight
+		finalHeight = (windowHeight - earlyAccessHeight)
+	else
+		finalHeight = minHeight
+	###
+
+	finalHeight = if ((windowHeight - earlyAccessHeight) > minHeight) then (windowHeight - earlyAccessHeight) else minHeight
+
+	$(".l-splash--container, .l-splash--image").height(finalHeight)
--- public/js/portfolio.js
+++ public/js/portfolio.js
@@ -1,4 +1,79 @@
 (function() {
-  $("#splash").backstretch("public/img/profile.jpg");
+  var resize_top_image, threshold;
+
+  $(function() {
+    resize_top_image();
+    $(window).on("resize", resize_top_image);
+    $("#header-about").on("click", function(e) {
+      var divY;
+      e.preventDefault();
+      divY = $(".l-about").position().top;
+      return $("html,body").animate({
+        scrollTop: divY
+      }, 500);
+    });
+    $("#learn-more").on("click", function(e) {
+      var divY;
+      e.preventDefault();
+      divY = $(".l-about").position().top;
+      return $("html,body").animate({
+        scrollTop: divY
+      }, 500);
+    });
+    $("#header-portfolio").on("click", function(e) {
+      var divY;
+      e.preventDefault();
+      divY = $(".l-portfolio").position().top;
+      return $("html,body").animate({
+        scrollTop: divY
+      }, 500);
+    });
+    $("#header-resume").on("click", function(e) {
+      var divY;
+      e.preventDefault();
+      divY = $(".l-resume").position().top;
+      return $("html,body").animate({
+        scrollTop: divY
+      }, 500);
+    });
+    return $("#header-connect").on("click", function(e) {
+      var divY;
+      e.preventDefault();
+      divY = $(".l-connect").position().top;
+      return $("html,body").animate({
+        scrollTop: divY
+      }, 500);
+    });
+  });
+
+  /*
+  	$(window).on "scroll", ->
+  		if($(@).scrollTop() >= threshold)
+  			$(".l-header").fadeIn 500 , ->
+  				$(@).removeClass "hide"
+  		else
+  			$(".l-header").fadeOut 200, ->
+  				$(@).addClass "hide"
+  */
+
+
+  threshold = 0;
+
+  resize_top_image = function() {
+    var earlyAccessHeight, finalHeight, minHeight, windowHeight;
+    windowHeight = $(window).height();
+    threshold = windowHeight;
+    earlyAccessHeight = $(".l-splash--nav").height();
+    minHeight = parseInt($(".l-splash--image").css("min-height"));
+    /*
+    	if (windowHeight - earlyAccessHeight) > minHeight
+    		finalHeight = (windowHeight - earlyAccessHeight)
+    	else
+    		finalHeight = minHeight
+    */
+
+    finalHeight = (windowHeight - earlyAccessHeight) > minHeight ? windowHeight - earlyAccessHeight : minHeight;
+    return $(".l-splash--container, .l-splash--image").height(finalHeight);
+  };
 
 }).call(this);
